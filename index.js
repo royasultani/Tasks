b920760904edf0c56f9bab31eae306a8fa6b28ad
Roya Sultani <royasulthani7@mail.ru>
2023-10-25 16:28:02 +0400

    init1

---
+++ index.js
@@ -0,0 +1,54 @@
+1️let i=3;
+let num1= i**2;
+let num2 = i**3;
+
+console.log(num1, num2)
+2️const V=60;
+const S=180;
+const zaman =((S/V)*60);
+console.log(zaman);
+3️
+let num1=50;
+let num2=90;
+let num3=30;
+
+if(num1>num2 && num1>num3)
+{
+    console.log(num1)
+}
+else if(num2>num1 && num2>num3)
+{
+    console.log(num2)
+}
+else{
+    console.log(num3)
+}
+4️
+let num = 3;
+
+if(num % 2 ===0){
+    console.log(“EVEN”)
+}
+else{
+    console.log(“ADD”)
+}
+5️
+const gun = 8; 
+
+if (gun === 1) {
+    console.log("Bazar ertesi");
+} else if (gun === 2) {
+    console.log("cerwenbe axwami");
+} else if (gun === 3) {
+    console.log("cerwenbe");
+} else if (gun === 4) {
+    console.log("cume axwami");
+} else if (gun === 5) {
+    console.log("cume");
+} else if (gun === 6) {
+    console.log("wenbe");
+} else if (gun === 7) {
+    console.log("bazar");
+} else {
+    console.log(" Qaqa Qardaşımsan Amerika kəşf eləmə!");
+}
